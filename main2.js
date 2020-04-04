c38a73dd062ae46c894997a5f41c1a5080bc85f9
unknown <trtuan900@gmail.com>
2020-04-04 15:27:41 +0700

    "test"

--- main2.js
+++ main2.js
@@ -5,11 +5,11 @@ const port =  process.env.PORTS || 8888
 app.get('/', (req, res) => {
     res.send('Welcome');
 })
-app.get('/home'),(req,res) => {
-    res.send('HOMEPAGE');
+app.get('/home',(req,res) => {
+    res.send('HOMEPAGE')
 })
-app.get('/login'),(req,res) => {
+app.get('/login',(req,res) => {
     res.send('<h1> Trang Login Dang Nhap tai khoan </h1>');
-}
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
