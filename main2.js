65c8b64d748d9090c2d1f1860e17abf776ac27b2
unknown <trtuan900@gmail.com>
2020-04-04 15:16:27 +0700

    "one"

---
+++ main2.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port =  process.env.PORTS
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+})
+app.get('/home'),(req,res) => {
+    res.send('HOMEPAGE');
+})
+app.get('/login'),(req,res) => {
+    res.send('<h1> Trang Login Dang Nhap tai khoan </h1>');
+}
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
